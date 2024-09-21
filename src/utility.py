3cfc3fd59e9ff899057ed5bff5d2e1701aa6bbfe
Bethlehem Dereselegn <b12derese@gmail.com>
2024-09-21 16:22:28 +0300

    Sales behavior

--- src/utility.py
+++ src/utility.py
@@ -165,6 +165,7 @@ class ANALYSIS:
 
     # correlation
     def sale_customer_corr(self):
+        self.info_log.info('Sales and Customers relation')
         new_df = self.df[['Sales','Customers']]
         print(new_df.corr())
 
@@ -174,4 +175,178 @@ class ANALYSIS:
         plt.xlabel('Customers')
         plt.ylabel('Sales')
         plt.show()
+        
+
+    # promo effect on sales
+    def promo_sale_behavior(self):
+        self.info_log.info('Promo effect on sales')
+        # sales
+        print('Sales during promo and non-promo days')
+        print(self.df.groupby('Promo')['Sales'].sum())
+
+        # number of customers
+        print('Number of Customer during promo and non-promo days')
+        print(self.df.groupby('Promo')['Customers'].sum())
+
+        # existing customers on promo and non-promo days
+        print('Stores with promo existing and new customers')
+        stores_with_promo = self.df[self.df['Promo'] == 1]['Store'].unique()
+
+        customers_during_nonpromo = self.df[(self.df['Store'].isin(stores_with_promo)) & (self.df['Promo'] == 0)]['Customers'].sum()
+        print(f'Number of existing customers: {customers_during_nonpromo}')
+
+        customers_during_promo = self.df[(self.df['Store'].isin(stores_with_promo)) & (self.df['Promo'] == 1)]['Customers'].sum()
+        print(f'Number of customers on promotion day: {customers_during_promo}')
+
+        # new customers
+        print(f'Number of new customers: {customers_during_promo - customers_during_nonpromo}')
+
+    # add day name 
+    def day_name(self):
+        self.df['dayName'] = np.where(self.df['DayOfWeek'] == 1, 'Monday',
+                                      np.where(self.df['DayOfWeek'] == 2, 'Tuesday',
+                                      np.where(self.df['DayOfWeek'] == 3, 'Wednesday',
+                                      np.where(self.df['DayOfWeek'] == 4, 'Thursday',
+                                      np.where(self.df['DayOfWeek'] == 5, 'Friday',
+                                      np.where(self.df['DayOfWeek'] == 6, 'Saturday',
+                                      np.where(self.df['DayOfWeek'] == 7, 'Sunday','Not a day')))))))
+    # day of promos 
+    def promo_days_behavior(self):
+        self.info_log.info('Days of the week most promos happen')
+        new_df = self.df[self.df['Promo'] == 1]
+        new_df = new_df.groupby('dayName').agg(
+            number_of_promo = ('Promo', 'count'),
+            sales = ('Sales', 'mean'),
+            customers = ('Customers', 'mean')
+        ).reset_index()
+
+        print(new_df)
+
+    # store type promo behavior(self)
+    def store_promo_deploy(self):
+        self.info_log.info('Which store type should promo be deployed')
+        new_df = self.df[self.df['Promo'] == 1]
+        new_df = new_df.groupby('StoreType').agg(
+            number_of_promo = ('Promo', 'count'),
+            sales = ('Sales', 'mean'),
+            customers = ('Customers', 'mean')
+        ).reset_index()
+
+        new_df['sale_per_customer'] = new_df['sales'] / new_df['customers']
+
+        print(new_df)
+
+    # customers trend on opening and closing time
+    def customer_time_behaviour(self):
+        self.info_log.info('Customers average purchases on opening time')
+        new_df = self.df[self.df['Open'] == 1]
+        new_df = new_df.groupby('dayName').agg(
+            sales = ('Sales', 'mean'),
+            customers = ('Customers', 'mean')
+        ).reset_index()
+
+        print(new_df)
+
+        # bar plot
+        sns.barplot(data=new_df, x='dayName', y='sales')
+        plt.title('Customer opening time trends')
+        plt.xlabel('Day')
+        plt.ylabel('Average sale')
+        plt.show()
+
+    # assortment effect on sale
+    def assortment_on_sale(self):
+        self.info_log.info('Assortments vs Sales')
+        new_df = self.df.groupby('Assortment')['Sales'].mean().reset_index()
+        new_df['Assortment'] = np.where(new_df['Assortment'] == 'a', 'Basic',
+                                        np.where(new_df['Assortment'] == 'b', 'Extra',
+                                        np.where(new_df['Assortment'] == 'c', 'Extended','None')))
+
+        print(new_df)
+
+        # bar plot
+        sns.barplot(data=new_df, x='Assortment', y='Sales')
+        plt.title('Assortment effect on sales')
+        plt.xlabel('Assortment')
+        plt.ylabel('Average sale')
+        plt.show()
+
+    # weekdays and weekends behaviour
+    def weekdays_weekends_behaviour(self):
+        self.info_log.info('Day of the week vs Sales')
+        stores = self.df[self.df['DayOfWeek'] == 1]['Store'].unique().tolist()
+ 
+        for i in range(2,6,1):
+            nextDay = self.df[self.df['DayOfWeek'] == i]['Store'].unique().tolist()
+            stores = list(set(stores).intersection(set(nextDay)))
+
+        if len(stores) == self.df['Store'].nunique():
+            print('All stores are open weekdays')
+
+            weekdays_sales = self.df[(self.df['DayOfWeek'] >= 1) & (self.df['DayOfWeek'] < 6)]['Sales'].mean()
+            weekdays_customers = self.df[(self.df['DayOfWeek'] >= 1) & (self.df['DayOfWeek'] < 6)]['Customers'].mean()
+
+            weekends_sales = self.df[(self.df['DayOfWeek'] >= 6) & (self.df['DayOfWeek'] < 8)]['Sales'].mean()
+            weekends_customers = self.df[(self.df['DayOfWeek'] >= 6) & (self.df['DayOfWeek'] < 8)]['Customers'].mean()
+
+            print(f'Weedays average sales: {weekdays_sales}')
+            print(f'Weedays average number of customers: {weekdays_customers}')
+            print(f'Weekends average sales: {weekends_sales}')
+            print(f'Weekends average number of customers: {weekends_customers}')
+
+        else:
+            print(f'Only {len(stores)} are open weekdays')
+
+            weekdays_sales = self.df[(self.df['DayOfWeek'] >= 1) & (self.df['DayOfWeek'] < 6) & (self.df['Store'].isin(stores))]['Sales'].mean()
+            weekdays_customers = self.df[(self.df['DayOfWeek'] >= 1) & (self.df['DayOfWeek'] < 6) & (self.df['Store'].isin(stores))]['Customers'].mean()
+
+            weekends_sales = self.df[(self.df['DayOfWeek'] >= 6) & (self.df['DayOfWeek'] < 8) & (self.df['Store'].isin(stores))]['Sales'].mean()
+            weekends_customers = self.df[(self.df['DayOfWeek'] >= 6) & (self.df['DayOfWeek'] < 8) & (self.df['Store'].isin(stores))]['Customers'].mean()
+
+            print(f'Weedays average sales: {weekdays_sales}')
+            print(f'Weedays average number of customers: {weekdays_customers}')
+            print(f'Weekends average sales: {weekends_sales}')
+            print(f'Weedays average number of customers: {weekends_customers}')
+
+    
+    # competitors distance effect
+    def competitor_distance_sale_behavior(self):
+        self.info_log.info('Competitor distance vs Sales')
+        new_df = self.df[['Store','CompetitionDistance']]
+        new_df.drop_duplicates(inplace=True)
+        mean_distance = new_df['CompetitionDistance'].mean()
+
+        # sales
+        sales_greater = self.df[self.df['CompetitionDistance'] > mean_distance]['Sales'].mean()
+        sales_less = self.df[self.df['CompetitionDistance'] <= mean_distance]['Sales'].mean()
+
+        print(f"Stores with greater than average competitor distance has average sales: {sales_greater}")
+        print(f"Stores with less than average competitor distance has average sales: {sales_less}")
+        print('\n')
+
+        # number of customers
+        cust_greater = self.df[self.df['CompetitionDistance'] > mean_distance]['Customers'].mean()
+        cust_less = self.df[self.df['CompetitionDistance'] <= mean_distance]['Customers'].mean()
+
+        print(f"Stores with greater than average competitor distance has average number of customers: {cust_greater}")
+        print(f"Stores with less than average competitor distance has average number of customers: {cust_less}")
+        print('\n')
+
+        # promo
+        df_promo_greater = self.df[self.df['CompetitionDistance'] > mean_distance]
+        promo_sale_greater = df_promo_greater[df_promo_greater['Promo'] == 1]['Sales'].sum()
+        df_promo_less = self.df[self.df['CompetitionDistance'] <= mean_distance]
+        promo_sale_less = df_promo_less[df_promo_less['Promo'] == 1]['Sales'].sum()
+        print('\n')
+
+        print(f"Stores with greater than average competitor distance has promotion sales: {promo_sale_greater}")
+        print(f"Stores with less than average competitor distance has promotion sales: {promo_sale_less}")
+
+
+        plt.scatter(self.df['CompetitionDistance'],self.df['Sales'])
+        plt.title("The relation between competitor distance and sales")
+        plt.xlabel('Competitor Distance')
+        plt.ylabel('Sales')
+        plt.show()
+
         
